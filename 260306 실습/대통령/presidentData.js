3f2a9826f0cf25ec246953302d09ab9f66fc11c0
이민주 <140781374+minju0236@users.noreply.github.com>
2026-03-18 15:06:20 +0900

    3월 첫째주 실습 파일

---
+++ 260306 실습/대통령/presidentData.js
@@ -0,0 +1,17 @@
+const presidentData = [
+
+    { id: 1, name: "이승만", term: "1-3대", period: "1948.07.24 ~ 1960.04.27", vote: "92.5%", status: "완료" },
+    { id: 2, name: "윤보선", term: "4대", period: "1960.08.13 ~ 1962.03.24", vote: "80.3%", status: "완료" },
+    { id: 3, name: "박정희", term: "5-9대", period: "1963.12.17 ~ 1979.10.26", vote: "46.6%", status: "완료" },
+    { id: 4, name: "최규하", term: "10대", period: "1979.12.06 ~ 1980.08.16", vote: "96.7%", status: "완료" },
+    { id: 5, name: "전두환", term: "11-12대", period: "1980.09.01 ~ 1988.02.24", vote: "99.9%", status: "완료" },
+    { id: 6, name: "노태우", term: "13대", period: "1988.02.25 ~ 1993.02.24", vote: "36.6%", status: "완료" },
+    { id: 7, name: "김영삼", term: "14대", period: "1993.02.25 ~ 1998.02.24", vote: "41.9%", status: "완료" },
+    { id: 8, name: "김대중", term: "15대", period: "1998.02.25 ~ 2003.02.24", vote: "40.3%", status: "완료" },
+    { id: 9, name: "노무현", term: "16대", period: "2003.02.25 ~ 2008.02.24", vote: "48.9%", status: "완료" },
+    { id: 10, name: "이명박", term: "17대", period: "2008.02.25 ~ 2013.02.24", vote: "48.7%", status: "완료" },
+    { id: 11, name: "박근혜", term: "18대", period: "2013.02.25 ~ 2017.03.10", vote: "51.6%", status: "중단" },
+    { id: 12, name: "문재인", term: "19대", period: "2017.05.10 ~ 2022.05.09", vote: "41.1%", status: "완료" },
+    { id: 13, name: "윤석열", term: "20대", period: "2022.05.10 ~ 2025.04.04", vote: "45.73%", status: "중단" },
+    { id: 13, name: "이재명", term: "21대", period: "2025.06.04 ~ ", vote: "49.42%", status: "진행 중" },
+];
